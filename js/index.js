f24e906b6f6d4ad1e0b7d690b473e0d231f53ee5
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-02-23 20:46:10 +0200

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,149 @@
+const ctx = document.querySelector(".js-chart").getContext("2d");
+const GLOBAL_MEAN_TEMPERATURE = 14;
+
+fetchData()
+  .then(parseData)
+  .then(getLabelsAndData)
+  .then(({ years, temps, northem, southem }) =>
+    drawChart(
+      years,
+      temps,
+      northem,
+      southem,
+      "rgba(255, 99, 132, 0.2)",
+      "rgba(255, 99, 132, 0.2)",
+      "rgba(255, 99, 132, 0.2)"
+    )
+  );
+
+function fetchData() {
+  return fetch("./ZonAnn.Ts+dSST.csv").then((response) => response.text());
+}
+
+function parseData(data) {
+  return Papa.parse(data, { header: true }).data;
+}
+
+function getLabelsAndData(data) {
+  return data.reduce(
+    (acc, entry) => {
+      acc.years.push(entry.Year);
+      acc.temps.push(Number(entry.Glob) + GLOBAL_MEAN_TEMPERATURE);
+      acc.northem.push(Number(entry.NHem) + GLOBAL_MEAN_TEMPERATURE);
+      acc.southem.push(Number(entry.SHem) + GLOBAL_MEAN_TEMPERATURE);
+      return acc;
+    },
+    { years: [], temps: [], northem: [], southem: [] }
+  );
+}
+
+function drawChart(
+  labels,
+  data1,
+  data2,
+  data3,
+  color1 = "rgba(255, 99, 132, 0.2)",
+  color2 = "rgba(132, 99, 255, 0.2)",
+  color3 = "rgba(99, 255, 145, 0.2)"
+) {
+  new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: "Global mean temperature,🌡",
+          data: data1,
+          backgroundColor: color1,
+          borderColor: "rgba(255, 99, 132, 1)",
+          borderWidth: 1,
+          fill: false,
+        },
+        {
+          label: "Global mean temperature,🌡",
+          data: data2,
+          backgroundColor: color2,
+          borderColor: "rgba(132, 99, 255, 1)",
+          borderWidth: 1,
+          fill: false,
+        },
+        {
+          label: "Global mean temperature,🌡",
+          data: data3,
+          backgroundColor: color3,
+          borderColor: "rgba(99, 255, 145, 1)",
+          borderWidth: 1,
+          fill: false,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              // beginAtZero: true,
+              callback(value, index, values) {
+                return value + "°";
+              },
+            },
+          },
+        ],
+      },
+    },
+  });
+}
+
+// function fetchData() {
+//   fetch("./ZonAnn.Ts+dSST.csv")
+//     .then((response) => response.text())
+//     .then((data) => {
+//       // console.log(Papa.parse(data, { header: true }).data);
+//       const parseData = Papa.parse(data, { header: true }).data;
+//       // const years = parseData.map((entry) => entry.Year);
+//       const mappedData = parseData.reduce(
+//         (acc, entry) => {
+//           acc.years.push(entry.Year);
+//           acc.temps.push(Number(entry.Glob) + GLOBAL_MEAN_TEMPERATURE);
+//           return acc;
+//         },
+//         { years: [], temps: [] }
+//       );
+//       console.log(mappedData);
+//       // const tempsGlob = parseData.map(
+//       //   (entry) => Number(entry.Glob) + GLOBAL_MEAN_TEMPERATURE
+//       // );
+//       // console.log(tempsGlob);
+
+//       new Chart(ctx, {
+//         type: "line",
+//         data: {
+//           labels: mappedData.years,
+//           datasets: [
+//             {
+//               label: "Global mean temperature,🌡",
+//               data: mappedData.temps,
+//               backgroundColor: "rgba(255, 99, 132, 0.2)",
+//               borderColor: "rgba(255, 99, 132, 1)",
+//               borderWidth: 1,
+//             },
+//           ],
+//         },
+//         // options: {
+//         //   scales: {
+//         //     yAxes: [
+//         //       {
+//         //         ticks: {
+//         //           beginAtZero: true,
+//         //         },
+//         //       },
+//         //     ],
+//         //   },
+//         // },
+//       });
+//     });
+// }
+
+// fetchData();
+
+// console.log(window);
